42514c3824901efeffa29d2520f9264272cfcd6a
kjh <79633378+htmlk-first@users.noreply.github.com>
2022-10-18 16:59:57 +0900

    Add files via upload

---
+++ pose-detector/pose-detector-main/src/Mediapipe.js
@@ -0,0 +1,120 @@
+import { useRef, useEffect, useState, useLayoutEffect } from "react";
+import { Camera } from "@mediapipe/camera_utils";
+// eslint-disable-next-line no-unused-vars
+import { Pose, VERSION, Results } from "@mediapipe/pose";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { getDefaultLandmarks } from "./custom/customLandmark";
+import { DEFAULT_POSE_CONNECTIONS } from "./custom/customPoseConnection";
+import { getInputArr } from "./service/input";
+import { detect } from "./service/tf";
+
+function useWindowSize() {
+	const [size, setSize] = useState([0, 0]);
+	useLayoutEffect(() => {
+		function updateSize() {
+			setSize([1152, 648]);
+		}
+		window.addEventListener("resize", updateSize);
+		updateSize();
+		return () => window.removeEventListener("resize", updateSize);
+	}, []);
+	return size;
+}
+
+/**
+ *
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Results} results
+ */
+const draw = async (ctx, results) => {
+	if (!results) return;
+	const { poseLandmarks } = results;
+	let landmark = getDefaultLandmarks(poseLandmarks);
+	const arr = getInputArr(landmark);
+
+	const cWidth = ctx.canvas.width;
+	const cHeight = ctx.canvas.height;
+	ctx.save();
+	ctx.clearRect(0, 0, cWidth, cHeight);
+	ctx.drawImage(results.segmentationMask, 0, 0, cWidth, cHeight);
+
+	ctx.drawImage(results.image, 0, 0, cWidth, cHeight);
+
+	ctx.globalCompositeOperation = "source-over";
+	drawConnectors(ctx, landmark, DEFAULT_POSE_CONNECTIONS, {	//랜드마크 선
+		color: "#00FF00",
+		lineWidth: 4,
+	});
+	drawLandmarks(ctx, landmark, {	// 랜드마크 점
+		color: "#FF0000",
+		lineWidth: 2,
+	});
+
+	await detect(arr);
+	ctx.restore();
+};
+
+const Canvas = (props) => {
+	const { results } = props;
+	const canvasRef = useRef(null);
+	const [width, height] = useWindowSize();
+	useEffect(() => {
+		/** @type {HTMLCanvasElement} */
+		const canvas = canvasRef.current;
+		canvas.width = width;
+		canvas.height = height;
+		const ctx = canvas.getContext("2d");
+		draw(ctx, results);
+	});
+
+	return <canvas ref={canvasRef} {...props} width={1280} height={720} />;
+};
+
+
+function Mediapipe() {
+	const [results, setResults] = useState(null);
+	const inputVideo = useRef();
+
+	useEffect(() => {
+		const pose = new Pose({
+			locateFile: (file) => {
+				return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${VERSION}/${file}`;
+				//   return `/pose/${file}`;
+			},
+		});
+		pose.setOptions({
+			modelComplexity: 1,
+			smoothLandmarks: true,
+			enableSegmentation: true,
+			smoothSegmentation: true,
+			minDetectionConfidence: 0.5,
+			minTrackingConfidence: 0.5,
+		});
+		pose.onResults(onResults);
+		pose.initialize();
+
+		function onResults(results) {
+			if (!results.poseLandmarks) return;
+			setResults(results);
+		}
+		const videoEl = inputVideo.current;
+		const camera = new Camera(videoEl, {
+			onFrame: async () => {
+				await pose.send({ image: videoEl });
+			},
+		});
+		camera.start();
+	}, []);
+	return (
+		<div className="video">
+			<video
+				ref={inputVideo}
+				playsInline
+				style={{ display: "none" }}
+			></video>
+			<Canvas results={results}></Canvas>
+		</div>
+	);
+}
+
+export default Mediapipe;
---
+++ pose-detector/pose-detector-main/src/service/tf.js
@@ -0,0 +1,79 @@
+import * as tf from "@tensorflow/tfjs";
+import { har } from "./model";
+import { scaler } from "./scikit";
+
+/**
+ * @param {number[][]} landmark
+ */
+const getJoint = (landmark) => tf.tensor(landmark);
+
+/**
+ * convert array to tensor applying index slicing
+ * @param {number[][]} array
+ * @param {number[]} vectorIdArr
+ * @returns
+ */
+const cv2Vec = (array, vectorIdArr) => {
+	const vArr = vectorIdArr.map((vId) => array[vId]);
+	return tf.tensor(vArr);
+};
+
+/**
+ * convert radian to degree
+ * @param {tf.Tensor} radians
+ * @returns
+ */
+const radian2degree = (radians) => {
+	const pi = tf.scalar(180 / Math.PI);
+	return tf.mul(radians, pi);
+};
+
+/**
+ *
+ * @param {number[][]} landmark
+ */
+const getPointAngle = async (landmark) => {
+	const v1Id = [4, 2, 2, 8, 10, 12, 3, 1, 1, 7, 9, 11];
+	const v2Id = [6, 4, 8, 10, 12, 16, 5, 3, 7, 9, 11, 15];
+	const v1 = cv2Vec(landmark, v1Id);
+	const v2 = cv2Vec(landmark, v2Id);
+
+	let v = tf.sub(v2, v1);
+	v = tf.div(v, (tf.norm(v, 'euclidean', [-1,])).reshape([12, 1]));
+	v = await v.array();
+
+	const newV1Id = [0, 1, 2, 3, 4, 6, 7, 8, 9, 10];
+	const newV2Id = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
+	const newV1 = cv2Vec(v, newV1Id);
+	const newV2 = cv2Vec(v, newV2Id);
+
+	// https://en.wikipedia.org/wiki/Einstein_notation
+	const radians = tf.acos(tf.einsum("nt, nt->n", newV1, newV2));
+	const degree = radian2degree(radians);
+	return degree;
+};
+
+/**
+ *
+ * @param {number[][]} landmark
+ * @returns {Promise<number[]>}
+ */
+export const detect = async (landmark) => {
+	if (har.model === undefined) return;
+	const seqLength = 15;
+	const joint = getJoint(landmark);
+	const angle = await getPointAngle(landmark);
+	const angleList = await angle.data();
+	har.countExercise(angleList);
+	const reshape_angle = angle.reshape([1, -1]);
+	const _scaled_angle = scaler(reshape_angle);
+	const scaled_angle = _scaled_angle.reshape([-1, 1]);
+	const seq = await tf.concat([joint.flatten(), scaled_angle.flatten()]).data();
+	if (har.seq.push(seq) <= seqLength) return;
+	har.seq.shift();
+	const input = tf.expandDims(tf.tensor(har.seq), 0);
+	const result = await har.predict(input);
+	har.result = result[1];
+	console.log(har.result);
+	return result;
+};
